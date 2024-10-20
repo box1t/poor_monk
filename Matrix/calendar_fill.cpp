eea6dd0ffa3af81b9b1eed9699c8f7c0abbeadec
box1t <nikita.seryj@mail.ru>
2024-10-20 14:45:33 +0300

    changes

--- Matrix/calendar_fill.cpp
+++ Matrix/calendar_fill.cpp
@@ -8,37 +8,37 @@
 #include <vector>
 #include <string>
 
-// Заполнение вектора строк с календарём
+// Заполнение календаря
 std::vector<std::string> fill_calendar(int first_week_day, int days_in_month) {
     std::vector<std::string> calendar_vec; // Вектор для хранения строк календаря
-    std::string current_line; // Текущая заполняемая строка
-    int day_counter = 1;
+    std::string buffer_string; // Буферная строка
+    int day_counter = 1; // Счетчик дней для заполнения текущей строки
 
-    // Добавляем пустые места в первую строку до начала первого дня
-    for (int i = 1; i < first_week_day; ++i) {
-        current_line += "   "; // Заполняем пробелами
+    // Добавляем пробелы в первую строку до начала первого дня
+    for (int i = 1; i < first_week_day; ++i) { // day_counter не связан с пробелами
+        buffer_string += "   ";
     }
 
-    // Заполнение календаря днями
+    // Заполнение текущей строки днями с учетом пробелов
     while (day_counter <= days_in_month) {
         if (day_counter < 10) {
-            current_line += " " + std::to_string(day_counter) + " "; // Добавляем пробел перед однозначным числом
+            buffer_string += " " + std::to_string(day_counter) + " "; // Однозначные числа
         } else {
-            current_line += std::to_string(day_counter) + " "; // Для двузначных чисел пробел не нужен перед числом
+            buffer_string += std::to_string(day_counter) + " "; // Двузначные числа
         }
 
-        // Если достигли конца недели (7 дней), добавляем строку в вектор и создаём новую
+        // Перенос заполненной (7 днями) строки в вектор
         if ((first_week_day + day_counter - 1) % 7 == 0) { 
-            calendar_vec.push_back(current_line);
-            current_line.clear(); // Очищаем строку для новой недели
+            calendar_vec.push_back(buffer_string);
+            buffer_string.clear(); // Очищаем строку для новой недели
         }
 
         ++day_counter;
     }
 
     // Добавляем оставшуюся строку, если она не пустая
-    if (!current_line.empty()) {
-        calendar_vec.push_back(current_line);
+    if (!buffer_string.empty()) {
+        calendar_vec.push_back(buffer_string);
     }
     return calendar_vec;
 }
