9293c6d24f37b28c63bb8996444dea066fcb158b
box1t <nikita.seryj@mail.ru>
2025-01-31 19:52:06 +0300

    modified solution

--- easy_tasks/smallest_banknotes_v2.cpp
+++ easy_tasks/smallest_banknotes_v2.cpp
@@ -30,3 +30,45 @@ int main() {
         }
     }
 }
+
+/*
+good alternative
+
+int main() {
+    // 1. Добавим проверку входных данных
+    int total_cash;
+    std::cin >> total_cash;
+    if (total_cash <= 0) {
+        std::cout << "Invalid input: amount should be positive" << std::endl;
+        return 1;
+    }
+
+    // 2. Сразу отсортируем банкноты по убыванию
+    std::vector<int> banknote_values = {100, 50, 10, 5, 2, 1};
+    
+    // 3. Создадим вектор для хранения результата с известным размером
+    std::vector<int> result_vec(banknote_values.size(), 0);
+
+    // 4. Улучшим цикл
+    for (size_t i = 0; i < banknote_values.size(); ++i) {
+        if (total_cash >= banknote_values[i]) {  // Более понятное условие
+            result_vec[i] = total_cash / banknote_values[i];
+            total_cash %= banknote_values[i];    // Используем оператор %
+            
+            // 5. Улучшим вывод
+            if (result_vec[i] > 0) {
+                std::cout << "Банкнот номиналом " << banknote_values[i] 
+                         << ": " << result_vec[i] << std::endl;
+            }
+        }
+    }
+
+    // 6. Проверка на успешность размена
+    if (total_cash != 0) {
+        std::cout << "Ошибка: не удалось разменять всю сумму" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+*/
