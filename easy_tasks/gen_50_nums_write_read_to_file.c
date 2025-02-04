fd9f553db3e56c0788e711a5439eddcbaac81703
box1t <nikita.seryj@mail.ru>
2025-02-04 23:50:58 +0300

    writing codes

--- easy_tasks/gen_50_nums_write_read_to_file.c
+++ easy_tasks/gen_50_nums_write_read_to_file.c
@@ -1,31 +1,39 @@
 
-// Generate 50 random numbers in [-0.5, 0.5] and save them to a file
+/* 
+
+Write a C program that generates 50 random numbers between -0.5 and 0.5 and
+writes them to the file rand.dat. 
+
+The first line of ran.dat contains the number of random numbers, 
+while the next 50 lines contain 50 random numbers.
+
+*/
 
 
 
 #include <stdio.h>
-#include <stdlib.h> // rand() здесь!
-#include <time.h>   // для srand()
+
+// этот код не дописан. здесь есть ошибки. вернись к этой задаче еще раз
+// но это твой код. поздравляю, ты уже можешь писать сам, с ошибками, и ускоряться, подглядывая в ответ.
+// и ты научился исправлять ошибки.
 
 int main() {
-    FILE *file_desc = fopen("myfile.txt", "w"); // Открываем файл на запись
-    if (!file_desc) { 
-        perror("Ошибка открытия файла");
-        return 1; 
+    FILE* file_desc = fopen("rand.dat", "w");
+    srand(time(NULL));
+    fprintf(file_desc, "%d", 50);
+    
+    for (size_t i = 0; i < 50; ++i) {
+        fprintf(file_desc, "%d", (rand() - 0.5)  );
     }
 
-    srand(time(NULL)); // Инициализируем rand()
+    fclose(file_desc);
 
-    for (int i = 0; i < 50; ++i) {
-        double value = (rand() / (double)RAND_MAX) - 0.5; // Генерируем число в [-0.5, 0.5]
-        fprintf(file_desc, "%.2f ", value); // Записываем с разделителем (пробел)
+    file_desc = fopen("rand.dat", "r");
+    char str = fgetc(file_desc);
+    while (str != EOF) {
+        printf("%c", str);
+        str = fgetc(file_desc); // что если так не сделать
     }
-
-    fclose(file_desc); // Закрываем файл
-    return 0;
+    fclose(file_desc);
 }
-
-
-
-// а теперь оформи чтение из файла.
 
